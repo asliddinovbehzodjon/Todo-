04132b22439753fc52d8224855807c7e6ea4e416
Behzod <behzodtuit@gmail.com>
2022-09-29 21:55:56 +0500

    todo-app

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&x&fhovkfcd1q11cw-y27=ig9l0y41s7b#1wpn=2!2lk^p4l4n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
